36846fb2c6398390178a0da48af9794654933fcc
Amir Shrestha <amir@bottle.com.np>
2020-01-09 12:54:28 +0545

    bug fixes

--- src/alarm_macro.py
+++ src/alarm_macro.py
@@ -4,74 +4,74 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-conf_file = {
-    'lambda': {
-        '4xx_count': {
-            'AlarmName': '4xxErrors',
-            'MetricName': 'Errors',
-            'EvaluationPeriods': '1',
-            'ComparisonOperator': 'GreaterThanThreshold',
-            "DimensionsName": 'FunctionName',
-            'Namespace': 'AWS/Lambda', 
-            'Period': '60',
-            'Statistic': 'Sum', 
-            'Threshold': 5,
-            'Unit': 'Count'
+def handler(event, context):
+    conf_file = {
+        'lambda': {
+            '4xx_count': {
+                'AlarmName': '4xxErrors',
+                'MetricName': 'Errors',
+                'EvaluationPeriods': '1',
+                'ComparisonOperator': 'GreaterThanThreshold',
+                "DimensionsName": 'FunctionName',
+                'Namespace': 'AWS/Lambda', 
+                'Period': '60',
+                'Statistic': 'Sum', 
+                'Threshold': 5,
+                'Unit': 'Count'
+            },
+            'invocations_count':{
+                'AlarmName': 'Invocations',
+                'MetricName': 'Invocations',
+                'EvaluationPeriods': '1',
+                'ComparisonOperator': 'GreaterThanThreshold',
+                "DimensionsName": 'FunctionName',
+                'Namespace': 'AWS/Lambda', 
+                'Period': '60',
+                'Statistic': 'Sum',
+                'Threshold': 5,
+                'Unit': 'Count'
+            },
+            'throttles_count': {
+                'AlarmName': 'Throttles',
+                'MetricName': 'Throttles',
+                'EvaluationPeriods': '1',
+                'ComparisonOperator': 'GreaterThanThreshold',
+                "DimensionsName": 'FunctionName',
+                'Namespace': 'AWS/Lambda', 
+                'Period': '60',
+                'Statistic': 'Sum', 
+                'Threshold': 5,
+                'Unit': 'Count'
+            } 
         },
-        'invocations_count':{
-            'AlarmName': 'Invocations',
-            'MetricName': 'Invocations',
-            'EvaluationPeriods': '1',
-            'ComparisonOperator': 'GreaterThanThreshold',
-            "DimensionsName": 'FunctionName',
-            'Namespace': 'AWS/Lambda', 
-            'Period': '60',
-            'Statistic': 'Sum',
-            'Threshold': 5,
-            'Unit': 'Count'
-        },
-        'throttles_count': {
-            'AlarmName': 'Throttles',
-            'MetricName': 'Throttles',
-            'EvaluationPeriods': '1',
-            'ComparisonOperator': 'GreaterThanThreshold',
-            "DimensionsName": 'FunctionName',
-            'Namespace': 'AWS/Lambda', 
-            'Period': '60',
-            'Statistic': 'Sum', 
-            'Threshold': 5,
-            'Unit': 'Count'
-        } 
-    },
-    'ec2': {
-        'cpu_utilization': {
-            'AlarmName': 'CPUUtilization',
-            'MetricName': 'CPUUtilization', 
-            'EvaluationPeriods': '5',
-            'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 
-            "DimensionsName": 'InstanceId',
-            'Namespace': 'AWS/EC2', 
-            'Period': '120', 
-            'Statistic': 'Average', 
-            'Threshold': '85', 
-            'Unit': 'Percent'
-        },
-        'status_check': {
-            'AlarmName':'StatusCheck',
-            'MetricName':'StatusCheckFailed_Instance', 
-            'EvaluationPeriods': '5', 
-            'ComparisonOperator': 'GreaterThanOrEqualToThreshold',                                                                       
-            "DimensionsName": 'InstanceId',
-            'Namespace': 'AWS/EC2', 
-            'Period': '120', 
-            'Statistic': 'Average',
-            'Threshold': '20', 
-            'Unit': 'Percent'
+        'ec2': {
+            'cpu_utilization': {
+                'AlarmName': 'CPUUtilization',
+                'MetricName': 'CPUUtilization', 
+                'EvaluationPeriods': '5',
+                'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 
+                "DimensionsName": 'InstanceId',
+                'Namespace': 'AWS/EC2', 
+                'Period': '120', 
+                'Statistic': 'Average', 
+                'Threshold': '85', 
+                'Unit': 'Percent'
+            },
+            'status_check': {
+                'AlarmName':'StatusCheck',
+                'MetricName':'StatusCheckFailed_Instance', 
+                'EvaluationPeriods': '5', 
+                'ComparisonOperator': 'GreaterThanOrEqualToThreshold',                                                                       
+                "DimensionsName": 'InstanceId',
+                'Namespace': 'AWS/EC2', 
+                'Period': '120', 
+                'Statistic': 'Average',
+                'Threshold': '20', 
+                'Unit': 'Percent'
+            }
         }
     }
-}
 
-def handler(event, context):
     alarm_dictionary = {}
     fragment = event["fragment"]
     monitoring_topic = os.environ['SNSTopic']
@@ -80,8 +80,10 @@ def handler(event, context):
     for resource in resources:
         logger.info('Searching {} for resource type'.format(resource))
         resource_json = resources[resource]
-        try:             
+        try:   
+            print("testing")          
             if resource_json['Type'] == 'AWS::Lambda::Function':
+                print("This is a lambda resource")
                 logger.info('Resource {} is a lambda function'.format(resource))
                 lambda_alarms = aws_alarms(resource,monitoring_topic,resource_json, conf_file['lambda'])
                 alarm_dictionary.update(lambda_alarms)
@@ -111,20 +113,22 @@ def handler(event, context):
     return resp
 
 def aws_alarms(resource, monitoring_topic, resource_json, conf):
+    print("aws_alarms")
     alarm_dict = {}
     for _,v in conf.items():
         resource_alarm = generate_alarm(resource, monitoring_topic, {
                                         'AlarmName': v['AlarmName'],
-                                        'MetricName': v['Errors'],
+                                        'MetricName': v['MetricName'],
                                         'EvaluationPeriods': v['EvaluationPeriods'],
                                         'ComparisonOperator': v['ComparisonOperator'],
                                         "Dimensions": [{"Name": v['DimensionsName'],"Value": {"Ref": f'{resource}'}}],
                                         'Namespace': v['Namespace'], 
                                         'Period': v['Period'],
-                                        'Statistic': v['Sum'], 
+                                        'Statistic': v['Statistic'], 
                                         'Threshold': v['Threshold'],
                                         'Unit': v['Unit']}, resource_json)
         alarm_dict.update(resource_alarm)
+    print("alarm_dict: ", alarm_dict)
     return alarm_dict
 
 def generate_alarm(resource,monitoring_topic,alarm,resource_json):
